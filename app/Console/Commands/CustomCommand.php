a3917a3845d0d8fd1b77f6ad5eaf34f58bc299ca
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-21 10:07:05 +0700

    change customCommand

--- app/Console/Commands/CustomCommand.php
+++ app/Console/Commands/CustomCommand.php
@@ -39,37 +39,9 @@ class CustomCommand extends Command
      */
     public function handle()
     {
-        $room_list = '';
-        // get all user
-        $users = DB::table('users')->select('id')->get();
-        // dd($users);
-        foreach ($users as $user) {
-            // get all room of user 
-            $rooms = DB::table('rooms')->select('id')->where('created_by', '=', $user->id)->get();
-
-            // create string room
-            foreach ($rooms as $room) {
-                $room_list .= $room->id . ',';
-            }
-            $room_list = rtrim($room_list,",");
-
-            // run sql count 
-            $sql = "SELECT SUM(price_order) AS 'order_price', CAST(updated_at AS DATE) as order_date, created_by";
-            $sql .= " FROM orders";
-            $sql .= " WHERE room_id IN (";
-            $sql .= $room_list;
-            $sql .= ")";
-            $sql .= " AND DATE(updated_at) = DATE(NOW() - INTERVAL 1 DAY) GROUP BY CAST(updated_at AS DATE), created_by";
-
-            $total_order_yesterday = DB::select($sql);
-
-            $order_history = new Order_History;
-            $order_history->order_total = $total_order_yesterday[0]->order_price;
-            $order_history->created_at = $total_order_yesterday[0]->order_date;
-            $order_history->created_by = $total_order_yesterday[0]->created_by;
-            $order_history->save();
-        }
-
-        $this->info('running');
+        $order = new Order_History;
+        $order->order_total = 1;
+        $order->created_by = 1;
+        $order->created_at = date("Y-m-d H:i:s");
     }
 }
