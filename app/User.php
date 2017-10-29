474608ebbfaf88a0c8015743a889745ccd3b0e13
tuantt <tran.thanh.tuan269@gmail.com>
2017-10-29 09:20:03 +0700

    add active to user

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'created_at', 'updated_at'
+        'name', 'email', 'password', 'status', 'created_at', 'updated_at'
 
     ];
 
